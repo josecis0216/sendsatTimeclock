c43996d69ed097234b42e89f2ece311abde877f6
Jose Cisneros <josecis0216@gmail.com>
2020-06-01 11:03:43 -0600

    styled timeclock interface, needs functionality

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace sendsatTimeclock
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
